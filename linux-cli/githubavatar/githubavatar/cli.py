6c5d21a5a06218ce7fcd8bc4cdfcb1f8cbcfdb2c
Lha2405 <rungururdee.wa.59@ubu.ac.th>
2017-12-01 14:54:02 +0700

    update

--- linux-cli/githubavatar/githubavatar/cli.py
+++ linux-cli/githubavatar/githubavatar/cli.py
@@ -15,7 +15,7 @@ def main(name, as_author):
     """Get Github Avatar"""
     #greet = 'Howdy' if as_cowboy else 'Hello'
     #click.echo('{0}, {1}.'.format(greet, name))
-    api_user_url = 'https://resizing.flixster.com/zt1Al_3bLFv_5OGXwg-camTxzmk=/300x300/v1.bjsxMjYxMjY5O2o7MTc1NjQ7MTIwMDszMDAwOzE1MDA'.format(name)
+    api_user_url = 'https://api.github.com/users/{}'.format(name)
     json = requests.get(api_user_url).json()
     req = requests.get(json['avatar_url'])
     if sys.version_info >= (3,0):
